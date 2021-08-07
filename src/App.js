f18a051766a178d7fae342a5928228013e3735e4
Yauhen <kimuandr@gmail.com>
2021-08-08 02:22:57 +0300

    less 37

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ const App = (props) => {
     <div className='app-wrapper-content'>
       <Route path="/dialogs" render={ () => <Dialogs state={props.state.dialogsPage} /> }/>
       <Route path="/profile" render={() => <Profile profilePage={props.state.profilePage}
-                                                  addPost={props.addPost}
-                                                  updateNewPostText={props.updateNewPostText}
+      addPost={props.addPost}
+      updateNewPostText={props.updateNewPostText}
        /> }/>
       <Route path='/news' render={() => <News /> }/>
       <Route path='/music' render={() => <Music /> }/>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,70 +1,75 @@
-let rerenderEntireTree = () => {
-  console.log('state was changed');
-}
-
-let state = { 
+let store = {
+_state : { 
 
   profilePage: {
 
- posts: [
-    { id: 1, post: "Hi, how are you?", like: 5 },
-    { id: 2, post: "Hi, it's my first post", like: 10 },
-    { id: 3, post: "Yo", like: 111 },
-    { id: 4, post: "I'll kill you!", like: 6 }
+  posts: [
+      { id: 1, post: "Hi, how are you?", like: 5 },
+      { id: 2, post: "Hi, it's my first post", like: 10 },
+      { id: 3, post: "Yo", like: 111 },
+      { id: 4, post: "I'll kill you!", like: 6 }
+    ],
+    newPostText: 'kimuandr@gmail.com',
+    },
+
+    dialogsPage: {
+
+  messages: [
+
+      { id: 1, message: "Hello" },
+      { id: 2, message: "How are you?" },
+      { id: 3, message: "Yo" },
+      { id: 4, message: "Yo" },
+      { id: 5, message: "Yo" },
+      { id: 6, message: "Yo" },
+    ],
+
+  
+  dialogs: [
+
+    { id: 1, name: "Kima" },
+    { id: 2, name: "Dima" },
+    { id: 3, name: "Andrew" },
+    { id: 4, name: "Valery" },
+    { id: 5, name: "Renata" },
+    { id: 6, name: "Mark" },
   ],
-  newPostText: 'kimuandr@gmail.com',
   },
 
-  dialogsPage: {
+  sidebar: {
 
-     messages: [
+  },
+  },
 
-    { id: 1, message: "Hello" },
-    { id: 2, message: "How are you?" },
-    { id: 3, message: "Yo" },
-    { id: 4, message: "Yo" },
-    { id: 5, message: "Yo" },
-    { id: 6, message: "Yo" },
-  ],
-
- 
- dialogs: [
-
-  { id: 1, name: "Kima" },
-  { id: 2, name: "Dima" },
-  { id: 3, name: "Andrew" },
-  { id: 4, name: "Valery" },
-  { id: 5, name: "Renata" },
-  { id: 6, name: "Mark" },
-],
+getState() {
+  return this._state;
 },
 
-sidebar: {
+  _callSubscriber() {
+    console.log('state was changed');
+  },
 
-},
+  addPost() {
+    let newPost = {
+      id: 5,
+      post: this._state.profilePage.newPostText,
+      like: 0
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
+
 };
 
-window.state = state;
-
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    post: state.profilePage.newPostText,
-    like: 0
-  };
-
-state.profilePage.posts.push(newPost);
-state.profilePage.newPostText = '';
-rerenderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-state.profilePage.newPostText = newText;
-rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
-
-export default state;
+window.store = store;
+export default store;
